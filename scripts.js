4b001edf57e99c625c08addf7daf656c4aebb8a3
XzSlippy <xxxdes333@gmail.com>
2025-05-19 15:44:33 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', () => {
+  const loader = document.getElementById('loader');
+  if(loader) {
+    loader.style.opacity = '0';
+    loader.style.transition = 'opacity 0.5s ease';
+    setTimeout(() => {
+      loader.style.display = 'none';
+    }, 600);
+  }
+});
