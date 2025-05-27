1728cf956863f8778b3c55e18c8e36849b6a12f3
XzSlippy <xxxdes333@gmail.com>
2025-05-27 17:11:12 -0300

    Add files via upload
    Signed-off-by: XzSlippy <xxxdes333@gmail.com>

--- scripts.js
+++ scripts.js
@@ -1,10 +1,22 @@
-window.addEventListener('load', () => {
-  const loader = document.getElementById('loader');
-  if(loader) {
-    loader.style.opacity = '0';
-    loader.style.transition = 'opacity 0.5s ease';
-    setTimeout(() => {
-      loader.style.display = 'none';
-    }, 600);
+AOS.init();
+
+function showModal(rank) {
+  document.getElementById('paymentModal').style.display = 'flex';
+  document.getElementById('modalText').innerText = 'Comprar rango ' + rank;
+}
+
+function closeModal() {
+  document.getElementById('paymentModal').style.display = 'none';
+}
+
+function switchLang() {
+  const langBtn = document.querySelector('.lang-switch');
+  const title = document.getElementById('title');
+  if (langBtn.innerText === 'EN') {
+    langBtn.innerText = 'ES';
+    title.innerText = 'Buy Ranks in Nexus MC';
+  } else {
+    langBtn.innerText = 'EN';
+    title.innerText = 'Compra Rangos en Nexus MC';
   }
-});
+}
